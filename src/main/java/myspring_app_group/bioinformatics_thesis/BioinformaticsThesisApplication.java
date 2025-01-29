456cfa57f1b8e06ee655baba32131f1f167e0e7a
Leoka2000 <reusoliveiraleo@gmail.com>
2025-01-29 06:45:01 +0100

    first commit

---
+++ src/main/java/myspring_app_group/bioinformatics_thesis/BioinformaticsThesisApplication.java
@@ -0,0 +1,13 @@
+package myspring_app_group.bioinformatics_thesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioinformaticsThesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioinformaticsThesisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myspring_app_group/bioinformatics_thesis/BioinformaticsThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package myspring_app_group.bioinformatics_thesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioinformaticsThesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
